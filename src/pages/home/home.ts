ca1479bedd5754012100c2fe8f5bdfb9663c4512
hamza567 <you@example.com>
2018-04-26 22:07:13 +0100

    active synchro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuPage } from '../menu/menu';
 import { AuthProvider } from '../../providers/auth/auth';
+import { FichePvProvider } from '../../providers/fiche-pv/fiche-pv';
+import { ReclamationProvider } from '../../providers/reclamation/reclamation';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +12,11 @@ import { AuthProvider } from '../../providers/auth/auth';
 export class HomePage {
   username = '';
   password = '';
-  constructor(public navCtrl: NavController, private _auth: AuthProvider) {
+  constructor(public navCtrl: NavController, private _auth: AuthProvider, private _synchroPv: FichePvProvider, private _synchroRec: ReclamationProvider) {
+    // synchro pv 
+    // synchro rec
+    this._synchroPv.Synchro();
+    this._synchroRec.Synchro();
   }
 
   openMenu() {
